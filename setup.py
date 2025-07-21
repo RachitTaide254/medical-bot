e4130e0e6e8f6f4c0d6074cd1b840f11389e607d
RachitTaide254 <rtaide254@gmail.com>
2025-07-21 23:26:46 +0530

    medical bot

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medi_bot genAI",
+    version="0.0.1",
+    author="Rachit Taide",
+    author_email="rtaide254@gmail.com",
+    packages=find_packages(),
+    install_requires = []
+)
